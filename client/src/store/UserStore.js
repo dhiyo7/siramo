5145c18939a4b02ab1bd6f67ac5a58dd2f99284b
Dani Damara <danny12march@gmail.com>
2018-05-17 13:33:07 +0700

    Adding MobX to Login and Signup

--- client/src/store/UserStore.js
+++ client/src/store/UserStore.js
@@ -1,15 +1,53 @@
 import { observable, computed } from 'mobx'
-import { db, User } from '../firebase'
+import { db, User } from './firebase'
+import { Alert } from 'react-native'
 
 class UserStore {
-  @observable user = {
-    email: 'Test'
+  @observable userData = {
+    uid: '',
+    email: ''
   }
 
   @observable isLogin = false
 
-}
+  firebaseLogin = (data, navigation) => {
+    User.signInWithEmailAndPassword(data.email, data.password)
+      .then(response => {
+        console.log(response.user.uid)
+        if (response.user) {
+          this.userData.uid = response.user.uid
+          this.userData.email = response.user.email
+          this.isLogin = true
+          console.log(this.userData)
+          console.log(this.isLogin)
+          navigation.navigate('Home')
+        }
+      })
+      .catch(err => console.log(err))
+  }
 
+  firebaseSignUp = (data, navigation) => {
+    // nanti harus bikin loading
+    console.log("Masuk sini ?")
+    User.createUserWithEmailAndPassword(data.email, data.password)
+      .then(response => {
+        console.log('Response Daftar', response.user.uid)
+        if (response.user) {
+          this.userData.uid = response.user.uid
+          this.userData.email = response.user.email
+          this.isLogin = true
+          navigation.navigate('Home')
+        }
+        // nanti bikin db baru di firebase kalo mau ada name alamat dll
+        // kalo berhasil langsung pindah ke home
+      })
+      .catch(err => {
+        Alert.alert(err.code, err.message)
+      })
+  }
+
+  // Logout Belum
+}
 
 const userStore = new UserStore() 
 export default userStore
