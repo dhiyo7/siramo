fb13f76b2d8383eccd7b8e455c224b731f3b528c
Dani Damara <danny12march@gmail.com>
2018-05-17 21:40:16 +0700

    Cronjob for data check

--- server/routes/farm.js
+++ server/routes/farm.js
@@ -2,8 +2,11 @@ var express = require('express');
 var router = express.Router();
 const Farm = require('../controllers/farm')
 const images = require('../middleware/images')
+// const ScheduleWatering = require('../runner')
+const ScheduleWatering = require('../runnerTest')
 
 /* GET users listing. */
 router.post('/',images.multer.single('image'),images.sendUploadToGCS,Farm.create)
+router.post('/watering', ScheduleWatering)
 
 module.exports = router;
