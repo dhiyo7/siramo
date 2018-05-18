af85dd9f333077c7f2a5b75f97954e4bdb2096a3
Dani Damara <danny12march@gmail.com>
2018-05-18 13:48:24 +0700

    Add Detail Tanaman, Loader, and Change UserStore

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -3,14 +3,27 @@ import {
   View, Text, StyleSheet
 } from 'react-native'
 
+// Sementara
+import FarmDetail from './farms/FarmDetail'
+
 class Home extends Component {
   render() {
     return (
-      <View>
-        <Text> Halaman Home </Text>
+      <View style={styles.mainContainer}>
+        <Text> Halaman Home ini ? </Text>
+        <FarmDetail />
       </View>
     )
   }
 }
+
+const styles = StyleSheet.create({
+  mainContainer: {
+    flex: 1,
+    backgroundColor: '#151e2d',
+    alignItems: 'center',
+    justifyContent: 'flex-start',
+  }
+})
 
 export default Home
