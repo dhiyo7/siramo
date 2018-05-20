95a031834a64d78edabb90aaf1b16a67c6c0cf36
Dani Damara <danny12march@gmail.com>
2018-05-20 09:43:59 +0700

    ChangeFarmDate styling and add Start & Stop Watering

--- client/src/components/farms/ButtonOnOff.js
+++ client/src/components/farms/ButtonOnOff.js
@@ -11,22 +11,36 @@ import UserStore from '../../store/UserStore'
 @inject('UserStore')
 class ButtonOnOff extends Component {
   render() {
+    const {ready_siram, triggerSiram} = this.props.siram
+    let txtButton = 'Start Watering'
+    let buttonStyles = styles.buttonContainerStart
+    if (ready_siram === 0) {
+      txtButton = 'Stop Watering'
+      buttonStyles = styles.buttonContainerStop
+    }
     return (
       <TouchableOpacity
-        style={styles.buttonContainer}
-        //onPress={UserStore.triggerSiram}
+        style={buttonStyles}
+        onPress={triggerSiram}
       >
-        <Text style={styles.buttonText}>ON / OFF SIRAM</Text>
+        <Text style={styles.buttonText}>{txtButton}</Text>
       </TouchableOpacity>
     )
   }
 }
 
 const styles = StyleSheet.create({
-  buttonContainer: {
-    backgroundColor: '#2980b6',
+  buttonContainerStart: {
+    backgroundColor: '#33691E',
     paddingVertical: 15,
-    marginBottom: 4
+    marginBottom: 4,
+    borderRadius: 4
+  },
+  buttonContainerStop: {
+    backgroundColor: '#f44242',
+    paddingVertical: 15,
+    marginBottom: 4,
+    borderRadius: 4
   },
   buttonText: {
     color: '#fff',
