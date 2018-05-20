03e06c7eb187e3cbf5aafd85993a333c2f95b8ad
Dani Damara <danny12march@gmail.com>
2018-05-21 04:22:42 +0700

    Testing component FarmDetail, button triggerSiram not done yet

--- client/tests/UserStore.test.js
+++ client/tests/UserStore.test.js
@@ -2,7 +2,6 @@ import React from 'react'
 import Enzyme, { mount, shallow } from 'enzyme'
 import Adapter from 'enzyme-adapter-react-16'
 import UserStore from '../src/store/UserStore'
-import UserStore from '../src/store/UserStore'
 import { User } from '../src/store/firebase';
 Enzyme.configure({ adapter: new Adapter() })
 
