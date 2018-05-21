fcc68f766c4daf803cafa547a0814f52c04e2394
MuhammadRamadiansyah <ramadiansyah@hotmail.com>
2018-05-21 20:33:21 +0700

    done change user id

--- server/runner.js
+++ server/runner.js
@@ -1,24 +1,23 @@
-// const CronJob = require('cron').CronJob
 const {db} = require('./controllers/firebase')
 const cron = require('node-cron')
 
-db.ref('/farms/17UFak7JqufG1RXUeVW30jwdfrQ2').once('value', snapshot => {
+db.ref('/farms/Hmyc0z9azhQbKE4mcv0NNZwDfPB3').once('value', snapshot => {
   let schedule = snapshot.val().cronSchedule
   let task = cron.schedule(schedule, function() {
     if(snapshot.val().automaticMode) {
-      db.ref('/farms/17UFak7JqufG1RXUeVW30jwdfrQ2').update({
+      db.ref('/farms/Hmyc0z9azhQbKE4mcv0NNZwDfPB3').update({
         ready_siram: 1
       })
     }   
   }, false);  
 
-  db.ref(`/farms/17UFak7JqufG1RXUeVW30jwdfrQ2`).on('value', snap => {
+  db.ref(`/farms/Hmyc0z9azhQbKE4mcv0NNZwDfPB3`).on('value', snap => {
     if( schedule !== snap.val().cronSchedule) {
       task.destroy()
       schedule = snap.val().cronSchedule
       task = cron.schedule(schedule, function() {
         if(snap.val().automaticMode){
-          db.ref('/farms/17UFak7JqufG1RXUeVW30jwdfrQ2').update({
+          db.ref('/farms/Hmyc0z9azhQbKE4mcv0NNZwDfPB3').update({
             ready_siram: 1
           })
         }
