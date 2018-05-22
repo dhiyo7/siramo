bf816fc197b49f328f8b55da96e983f4cd6a27c5
Dani Damara <danny12march@gmail.com>
2018-05-22 16:14:21 +0700

    fitur push notif almost done

--- client/App.js
+++ client/App.js
@@ -1,34 +1,43 @@
-// import React from 'react';
-// import { StyleSheet, Text, View } from 'react-native';
-// import { createStackNavigator } from 'react-navigation'
-// import Farm from './src/components/Farm'
-// import AddFarm from './src/components/AddFarm'
-
-// const Navigator = createStackNavigator({
-//   Farm:{
-//     screen:Farm,
-//     navigationOptions: {
-//       headerTitle: 'Farm',
-//     }
-//   },
-//   AddFarm:{
-//     screen:AddFarm,
-//     navigationOptions: {
-//       headerTitle: 'AddFarm'
-//     }
-//   }
-// },{
-//   initialRouteName: 'Farm',
-// })
 import React from 'react'
 import { Provider } from 'mobx-react'
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, Alert } from 'react-native'
 import { createStackNavigator } from 'react-navigation'
 import RootStack from './src/components/stack/RootStack'
 import UserStore from './src/store/UserStore'
 import FarmStore from './src/store/FarmStore'
+import { db } from './src/store/firebase'
+import { Permissions, Notifications } from 'expo'
+import userStore from './src/store/UserStore';
+
+async function registerForPushNotificationsAsync() {
+  const {status: existingStatus} = await Permissions.getAsync(
+    Permissions.NOTIFICATIONS
+  )
+  let finalStatus = existingStatus
+  if (existingStatus !== 'granted') {
+    const { status } = await Permissions.askAsync(Permissions.NOTIFICATIONS)
+    finalStatus = status
+  }
+
+  if (finalStatus !== 'granted') {
+    return
+  }
+
+  let token = await Notifications.getExpoPushTokenAsync()
+  // console.log(token)
+  // let uid = userStore.userData.uid
+  userStore.userData.token = token
+  // if (uid) {
+  //   db.ref(`/farms/${uid}`).update({token})
+  // }
+}
 
 export default class App extends React.Component {
+  componentWillMount() {
+    registerForPushNotificationsAsync()
+    // this.listener = Notification.addListener(this.listen)
+  }
+  
   render() {
     return (
       <Provider UserStore={UserStore} FarmStore={FarmStore}>
--- client/src/store/UserStore.js
+++ client/src/store/UserStore.js
@@ -7,6 +7,7 @@ import FarmStore from '../store/FarmStore'
 
 class UserStore {
   @observable userData = {
+    token: '',
     uid: '',
     email: '',
     loading: false
@@ -15,8 +16,10 @@ class UserStore {
   @observable isLogin = false
 
   assignUserData = (data) => {
-      this.userData.uid = data.uid
-      this.userData.email = data.email
+    this.userData.uid = data.uid
+    this.userData.email = data.email
+    let token = this.userData.token
+    db.ref(`/farms/${data.uid}`).update({token})
   }
 
   firebaseLogin = (data, navigation) => {
@@ -26,8 +29,11 @@ class UserStore {
         .then( async(response) => {
           // console.log(response.user.uid)
           if (response.user) {
-            this.userData.uid = response.user.uid
-            this.userData.email = response.user.email
+            // this.userData.uid = response.user.uid
+            // this.userData.email = response.user.email
+            let uid = response.user.uid
+            let email = response.user.email
+            this.assignUserData({uid, email})
             this.isLogin = true
             this.userData.loading = false
             await FarmStore.getFarmData()
@@ -52,8 +58,9 @@ class UserStore {
       User.createUserWithEmailAndPassword(data.email, data.password)
         .then(response => {
           if (response.user) {
-            this.userData.uid = response.user.uid
-            this.userData.email = response.user.email
+            let uid = response.user.uid
+            let email = response.user.email
+            this.assignUserData({uid, email})
             this.isLogin = true
             this.userData.loading = false
             // FarmStore.getFarmData()
