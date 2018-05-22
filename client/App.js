435f0147628c38d11bf4f64a5032240f011242ea
MuhammadRamadiansyah <ramadiansyah@hotmail.com>
2018-05-22 20:29:37 +0700

    done splash screen

--- client/App.js
+++ client/App.js
@@ -1,35 +1,38 @@
-// import React from 'react';
-// import { StyleSheet, Text, View } from 'react-native';
-// import { createStackNavigator } from 'react-navigation'
-// import Farm from './src/components/Farm'
-// import AddFarm from './src/components/AddFarm'
-
-// const Navigator = createStackNavigator({
-//   Farm:{
-//     screen:Farm,
-//     navigationOptions: {
-//       headerTitle: 'Farm',
-//     }
-//   },
-//   AddFarm:{
-//     screen:AddFarm,
-//     navigationOptions: {
-//       headerTitle: 'AddFarm'
-//     }
-//   }
-// },{
-//   initialRouteName: 'Farm',
-// })
 import React from 'react'
 import { Provider } from 'mobx-react'
-import { StyleSheet, Text, View } from 'react-native'
+import { AsyncStorage } from 'react-native'
+import { NavigationAction } from 'react-navigation'
 import { createStackNavigator } from 'react-navigation'
 import RootStack from './src/components/stack/RootStack'
 import UserStore from './src/store/UserStore'
 import FarmStore from './src/store/FarmStore'
+import { Asset, AppLoading } from 'expo';
 
 export default class App extends React.Component {
+  // constructor() {
+  //   super()
+  //   this.state = {
+  //     isReady: false
+  //   }
+  // }
+
+  // _cacheResourcesAsync = async() => {
+  //   console.log('kesainiiii', await AsyncStorage.getItem('userId'))
+  //   await AsyncStorage.getItem('userId') == null?
+  //   console.log("ke login", this.props): console.log("kenhome")
+  // }
+
   render() {
+
+    // if(!this.state.isReady){
+    //   return (
+    //     <AppLoading
+    //       startAsync={this._cacheResourcesAsync}
+    //       onFinish={() => this.setState({ isReady: true })}
+    //       onError={console.warn}
+    //     />
+    //   );
+    // }
     return (
       <Provider UserStore={UserStore} FarmStore={FarmStore}>
         <RootStack/>
@@ -37,12 +40,3 @@ export default class App extends React.Component {
     )
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#151e2d',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-})
