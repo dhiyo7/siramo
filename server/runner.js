9ca0167d32b2ec9100cb9b145770d34ff8a46afd
MuhammadRamadiansyah <ramadiansyah@hotmail.com>
2018-05-22 19:14:45 +0700

    add axios and fixed content type

--- server/runner.js
+++ server/runner.js
@@ -1,10 +1,20 @@
 const {db} = require('./controllers/firebase')
 const cron = require('node-cron')
+const axios = require('axios')
 
 db.ref('/farms/Hmyc0z9azhQbKE4mcv0NNZwDfPB3').once('value', snapshot => {
   let schedule = snapshot.val().cronSchedule
   let task = cron.schedule(schedule, function() {
-    if(snapshot.val().automaticMode && snapshot.val().water_ratio <= snapshot.val().minWaterRatio) {
+    if(snapshot.val().automaticMode) {
+      axios.post('https://expo.io/--/api/v2/push/send', {
+        to: 'ExponentPushToken[vCQT7pCk00R2eSOQRxBxX_]',
+        title: 'Automated Watering ON!',
+        body: 'Your plant has been watered now'
+      }, {
+        Accept: 'application/json',
+        [Accept-Encoding]: 'gzip, deflate',
+        [Content-Type]: ' application/json/x-www-form-urlencoded'
+      })
       db.ref('/farms/Hmyc0z9azhQbKE4mcv0NNZwDfPB3').update({
         ready_siram: 1
       })
@@ -19,6 +29,15 @@ db.ref('/farms/Hmyc0z9azhQbKE4mcv0NNZwDfPB3').once('value', snapshot => {
         if(snap.val().automaticMode){
           db.ref('/farms/Hmyc0z9azhQbKE4mcv0NNZwDfPB3').update({
             ready_siram: 1
+          })
+          axios.post('https://expo.io/--/api/v2/push/send', {
+            to: 'ExponentPushToken[vCQT7pCk00R2eSOQRxBxX_]',
+            title: 'Automated Watering ON!',
+            body: 'Your plant has been watered now'
+          }, {
+            Accept: 'application/json',
+            [Accept-Encoding]: 'gzip, deflate',
+            [Content-Type]: ' application/json/x-www-form-urlencoded'
           })
         }
       }, false);
