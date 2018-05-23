a36a7aea42fc36c0ad417f933e2dba8398187887
MuhammadRamadiansyah <ramadiansyah@hotmail.com>
2018-05-23 17:01:58 +0700

    fix

--- raspi-server/index.js
+++ raspi-server/index.js
@@ -8,8 +8,13 @@ const board = new five.Board([
 	{id: 'pi', io: new Raspi(), debug: true},
 	{id: 'uno', port:'/dev/ttyACM0'}
 ])
-let dummyData,
-	 sensorIteration = 0
+let readySiram,
+	 waterLevel,
+	 minWaterRatio,
+	 maxWaterRatio,
+	 automaticMode,
+	 sensorIteration = 0,
+	 sensorWaterIteration = 0
 	 
 var tempSensor = {
   initialize: function() {
@@ -25,7 +30,7 @@ var tempSensor = {
     setTimeout(function(){
       tempSensor.read();
     }, 5000)
-      db.ref('/farms/17UFak7JqufG1RXUeVW30jwdfrQ2').update({
+      db.ref('/farms/Hmyc0z9azhQbKE4mcv0NNZwDfPB3').update({
 		temperature: temperature,
 		humidity: humidity
 	  })
@@ -34,32 +39,49 @@ var tempSensor = {
 board.on('ready', function() {
 	const relay = new five.Relay(10)
 	const moisture = new five.Sensor('A0')
+	const waterLevelSensor = new five.Sensor('A1')
 	this.repl.inject({
 		relay: relay
 	})
-	db.ref('/farms/17UFak7JqufG1RXUeVW30jwdfrQ2').on('value', (snapshot) => {
-		dummyData = snapshot.val().ready_siram
-		if(dummyData == 1) {
-			relay.open()
-		} else {
+	db.ref('/farms/Hmyc0z9azhQbKE4mcv0NNZwDfPB3').on('value', (snapshot) => {
+		readySiram = snapshot.val().ready_siram
+		waterLevel = snapshot.val().water_level
+		minWaterRatio = snapshot.val().minWaterRatio
+		maxWaterRatio = snapshot.val().maxWaterRatio
+		waterRatio = snapshot.val().water_ratio
+		automaticMode = snapshot.val().automaticMode
+		if(readySiram == 1) {
 			relay.close()
-			console.log(snapshot.val().water_ratio)
-			/*db.ref('/farms/17UFak7JqufG1RXUeVW30jwdfrQ2').update({
-				last_siram: firebase.database.ServerValue.TIMESTAMP
-			})*/
+			if(waterRatio > maxWaterRatio && automaticMode == true) {
+				db.ref('/farms/Hmyc0z9azhQbKE4mcv0NNZwDfPB3').update({
+					ready_siram: 0
+				})
+			}
+		} else {
+			relay.open()
 		}
 	})
 	
 	moisture.scale(0, 100).on('change', function () {
 			sensorIteration++
-			if(sensorIteration == 150) {
+			if(sensorIteration == 30) {
 				sensorIteration = 0
-				db.ref('/farms/17UFak7JqufG1RXUeVW30jwdfrQ2').update({
+				db.ref('/farms/Hmyc0z9azhQbKE4mcv0NNZwDfPB3').update({
 						water_ratio: 100 - this.value
 					})
 			}
 		})
-		
+	
+	waterLevelSensor.scale(0, 100).on('change', function () {
+			sensorWaterIteration++
+			if(sensorWaterIteration == 30){
+				sensorWaterIteration = 0
+				db.ref('/farms/Hmyc0z9azhQbKE4mcv0NNZwDfPB3').update({
+					water_level: this.value
+				})
+			}
+			
+		})
 	 if (tempSensor.initialize()) {
 		tempSensor.read();
 
